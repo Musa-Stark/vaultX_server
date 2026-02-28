1332de3b1aabc6fc1b94971c3ac8892dee487507
Musa-Stark <musa.fullstack08@gmail.com>
2026-02-28 18:50:58 +0500

    Password valdiation disabled for items

--- modules/password/password.validation.js
+++ modules/password/password.validation.js
@@ -3,7 +3,5 @@ export const passwordValidation = (data) => {
   if (!service || !username || !password || !category || !icon)
     return "Service, username, password, category and icon are required";
 
-  if (password.length < 6) return "Password must be at least 6 characters";
-
   return null;
 };
